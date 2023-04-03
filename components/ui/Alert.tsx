2d1960933d2776a3990c89f1e11d98e986020446
Leafmire <ekwkqdk007@naver.com>
2023-04-03 19:50:18 +0900

    [ADD] Authentication functions

--- components/ui/Alert.tsx
+++ components/ui/Alert.tsx
@@ -2,7 +2,7 @@ import { XCircleIcon } from '@heroicons/react/20/solid'
 
 export default function Alert({title, messages}: {title: string, messages: string[]}) {
   return (
-    <div className="rounded-md bg-red-50 p-4">
+    <div className="w-full max-w-lg rounded-md bg-red-50 p-4">
       <div className="flex">
         <div className="flex-shrink-0">
           <XCircleIcon className="h-5 w-5 text-red-400" aria-hidden="true" />
