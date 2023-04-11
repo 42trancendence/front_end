b26228271a05165ac2883fbd9f9358b3a4dcfc6b
guswlsakfls <guswlsakfls@gmail.com>
2023-04-11 19:31:20 +0900

    [TEST] test

---
+++ pages/lobby/game/socketio.tsx
@@ -0,0 +1,13 @@
+import io from 'socket.io-client';
+
+const socket = io('http://localhost:3000');
+
+socket.on('connect', () => {
+  console.log('Connected to server');
+});
+
+socket.on('disconnect', () => {
+  console.log('Disconnected from server');
+});
+
+export default socket;
