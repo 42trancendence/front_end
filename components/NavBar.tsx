15b0112d539c0306be2e1339fef95ed60d4c0e26
Leafmire <ekwkqdk007@naver.com>
2023-04-13 17:07:24 +0900

    [ADD] chat socket status 변경

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -7,9 +7,18 @@ import {
 	UsersIcon,
 } from "@heroicons/react/24/outline";
 import logoPic from "@/public/42_Logo.svg";
+import { useRouter } from "next/router";
+import { useContext, useEffect } from "react";
+import { SocketContext } from "@/lib/socketContext";
 
 function classNames(...classes: string[]) {
 	return classes.filter(Boolean).join(" ");
+}
+
+interface NavigationItem {
+	name: string;
+	href: string;
+	icon: any;
 }
 
 const navigation = [
@@ -17,15 +26,44 @@ const navigation = [
 		name: "내 정보",
 		href: "/lobby/overview",
 		icon: IdentificationIcon,
-		current: true,
 	},
-	{ name: "채팅", href: "/lobby/chat", icon: UsersIcon, current: false },
-	{ name: "게임", href: "/lobby/game", icon: PlayIcon, current: false },
+	{ name: "채팅", href: "/lobby/chat", icon: UsersIcon },
+	{ name: "게임", href: "/lobby/game", icon: PlayIcon },
 ];
 
-export default function NavBar({userData}: any) {
+
+
+function NavItem({ item }: { item: NavigationItem }) {
+	let isActive = useRouter().pathname === item.href;
+
 	return (
-		<div className="flex flex-col w-72 h-screen sticky top-0 border-r border-zinc-700">
+			<Link
+				href={item.href}
+				className={classNames(
+					isActive
+						? "bg-white text-zinc-800"
+						: "text-indigo-200 hover:bg-zinc-700 hover:text-white",
+					"group flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6"
+				)}
+			>
+				<item.icon
+					className={classNames(
+						isActive
+							? "text-zinc-800"
+							: "text-indigo-200 group-hover:text-white",
+						"h-6 w-6 shrink-0"
+					)}
+					aria-hidden="true"
+				/>
+				{item.name}
+			</Link>
+	);
+}
+
+export default function NavBar({ userData }: any) {
+
+	return (
+		<div className="sticky top-0 flex h-screen w-72 flex-col border-r border-zinc-700">
 			<div className="flex grow flex-col gap-y-5 overflow-y-auto bg-zinc-900 px-6">
 				<div className="flex h-16 shrink-0 items-center">
 					<Image className="h-8 w-auto" src={logoPic} alt="Your Company" />
@@ -36,26 +74,7 @@ export default function NavBar({userData}: any) {
 							<ul role="list" className="-mx-2 space-y-1">
 								{navigation.map((item) => (
 									<li key={item.name}>
-										<Link
-											href={item.href}
-											className={classNames(
-												item.current
-													? "bg-white text-zinc-800"
-													: "text-indigo-200 hover:text-white hover:bg-zinc-700",
-												"group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold"
-											)}
-										>
-											<item.icon
-												className={classNames(
-													item.current
-														? "text-zinc-800"
-														: "text-indigo-200 group-hover:text-white",
-													"h-6 w-6 shrink-0"
-												)}
-												aria-hidden="true"
-											/>
-											{item.name}
-										</Link>
+										<NavItem item={item} />
 									</li>
 								))}
 							</ul>
@@ -64,7 +83,10 @@ export default function NavBar({userData}: any) {
 							<div className="text-xs font-semibold leading-6 text-indigo-200">
 								친구
 							</div>
-							<ul role="list" className="-mx-2 mt-2 space-y-1 p-2 bg-zinc-800 rounded-md">
+							<ul
+								role="list"
+								className="-mx-2 mt-2 space-y-1 rounded-md bg-zinc-800 p-2"
+							>
 								<FreindList userData={userData} />
 							</ul>
 						</li>
