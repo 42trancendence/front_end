25a777909cdc2068620a59b991936ccfc810cbcc
Leafmire <ekwkqdk007@naver.com>
2023-04-13 16:00:38 +0900

    [ADD] freindActive 소켓 이벤트

--- components/FriendList.tsx
+++ components/FriendList.tsx
@@ -3,24 +3,29 @@ import Image from "next/image";
 import DefaultAvatarPic from "@/public/default_avatar.svg";
 import clsx from "clsx";
 
-export default function FreindList({userData}: any) {
+export default function FreindList({ userData }: any) {
 	return (
 		<>
 			{userData.map((user: any, index: number) => (
-			<li key={index}>
-				<Link
-					href="#"
-					className="group flex items-center gap-x-4 rounded-md p-2 text-sm font-normal leading-6 text-indigo-200 hover:bg-zinc-700 hover:text-white"
-				>
-					<Image
-						className="inline-block h-7 w-7 flex-none rounded-full"
-						src={DefaultAvatarPic}
-						alt=""
-					/>
-					<span className="flex-auto truncate">{user.name}</span>
-					<span className="h-2 w-2 flex-none rounded-full bg-green-500"></span>
-				</Link>
-			</li>
+				<li key={index}>
+					<Link
+						href="#"
+						className="group flex items-center gap-x-4 rounded-md p-2 text-sm font-normal leading-6 text-indigo-200 hover:bg-zinc-700 hover:text-white"
+					>
+						<Image
+							className="inline-block h-7 w-7 flex-none rounded-full"
+							src={DefaultAvatarPic}
+							alt=""
+						/>
+						<span className="flex-auto truncate">{user.name}</span>
+						<span
+							className={clsx(
+								user.status === true ? "bg-green-500" : "bg-red-500",
+								"h-2 w-2 flex-none rounded-full"
+							)}
+						></span>
+					</Link>
+				</li>
 			))}
 		</>
 	);
