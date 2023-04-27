bf9dcc1807d2eb05ff16eece62213f2643f591f4
guswlsakfls <guswlsakfls@gmail.com>
2023-04-27 15:27:59 +0900

    [FEAT] 새로고침 문제, 모드 기능

--- components/canvas/canvas.tsx
+++ components/canvas/canvas.tsx
@@ -1,12 +1,14 @@
 import React, { RefObject, useEffect, useRef, useState, useContext } from 'react'
 import { GameSocketContext } from '@/lib/socketContext';
+import usePersistentState from "@/components/canvas/usePersistentState";
 
 const Canvas: React.FC = () => {
   const canvasRef: RefObject<HTMLCanvasElement> = useRef(null);
   const [ctx, setCtx] = useState<CanvasRenderingContext2D | null>(null);
   const [gameData, setGameData] = useState(null);
   const [ready, setReady] = useState(false);
-  const [startGame, setStartGame] = useState(false);
+  const [startGame, setStartGame] = usePersistentState('startGame', false);
+  const [difficulty, setDifficulty] = useState(false);
 
   // 컨텍스트 세팅
   useEffect(() => {
@@ -16,38 +18,37 @@ const Canvas: React.FC = () => {
       
       setCtx(context);
     }
-  }, [canvasRef]);
+  }, []);
 
   // 소켓 연결(컨텍스트 세팅, socket.id 가 초기화 되는지 확인 필요)
 	const { socket } = useContext(GameSocketContext);
   useEffect(() => {
-    console.log(socket);
+    // console.log(socket);
     if (socket) {
       socket.on('updateGame', (data) => {
-        // console.log(data);
+
         setGameData(data);
       })
-      socket.on('startGame', () => {
-        setStartGame(true);
-        console.log('startGame')
+      socket.on('setStartGame', (data) => {
+        if (data == 'start') {
+          setStartGame(true);
+        } else {
+          setStartGame(false);
+          alert(data);
+        }
+        console.log(`setStartGame: ${data}`);
       })
-      socket.on('postLeaveGame', () => {
-        setStartGame(false);
-        socket.emit('postLeaveGame');
-        console.log('getLeaveGame')
+      socket.on('getWatcher', (data) => {
+        console.log('getWatcher', data);
       })
     }
-  }, [socket, startGame])
+  }, [socket, setStartGame, setReady])
 
   // 컨텍스트가 세팅되면 그림 그리기
   useEffect(() => {
-    // if (ctx) {
-    //   ctx.fillStyle = 'white';
-    //   ctx.fillRect(0, 0, 500, 500);
-    // };
     render();
-  }, [ctx, gameData]);
-  
+  }, [ctx, gameData, startGame]);
+
   const render = () => {
     if (ctx && gameData) {
       ctx.clearRect(0, 0, 500, 500);
@@ -93,15 +94,30 @@ const Canvas: React.FC = () => {
     } 
   }
 
-  const handleReady = () => {
+  const handleReadyGame = () => {
     setReady(!ready);
     if (socket) {
-      socket.emit('postReady', !ready);
+      socket.emit('postReadyGame');
+    }
+  }
+
+  const hadleDifficulty = () => {
+    if (socket) {
+      if (difficulty) {
+        socket.emit('postDifficulty', difficulty ? 'normal' : 'hard');
+      }
+      setDifficulty(!difficulty);
+    }
+  }
+
+  const hadleLeaveGame = () => {
+    if (socket) {
+      socket.emit('postLeaveGame');
     }
   }
 
   return (
-    startGame ? 
+    <>
       <div
         tabIndex={0} // 키보드 포커스를 위한 tabIndex 설정
         style={{ outline: 'none' }} // 선택시 브라우저가 테두리를 그리지 않도록 함
@@ -109,17 +125,48 @@ const Canvas: React.FC = () => {
       >
         <canvas 
           ref={canvasRef} width={500} height={500}
-          />
+        />
       </div>
-      :
-      <div>
-        <button 
-          onClick={() => handleReady()}
-          className={'bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'}
-        >
-          {ready ? '준비완료' : '준비' }
-        </button>
-      </div>
+      {startGame ? 
+        <div>
+          게임 시작
+        </div>
+        :
+        // 가운데 정렬
+        <div className='mx-auto my-auto space-y-4'>
+          <div>
+            <div className='text-center mb-2'>
+              난이도
+            </div>
+            <div className='text-center'>
+              <button
+                onClick={() => hadleDifficulty()}
+                className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'
+              >
+                {difficulty ? 'hard' : 'normal' }
+              </button>
+            </div>
+          </div>
+
+          <div
+              className='space-x-4'
+          >
+            <button
+              onClick={() => handleReadyGame()}
+              className={'bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'}
+            >
+              {ready ? '준비완료' : '준비' }
+            </button>
+            <button
+              onClick={() => hadleLeaveGame()}
+              className={'bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'}
+            >
+              나가기
+            </button>
+          </div>
+        </div>
+    }
+    </>
   )
 }
 
--- pages/lobby/game/index.tsx
+++ pages/lobby/game/index.tsx
@@ -5,18 +5,14 @@ import { NextPageWithLayout } from "@/pages/_app";
 import { useContext } from "react";
 import { ReactElement, useEffect } from "react";
 import { useState } from "react";
-import Canvas from "../../../components/canvas";
+import Canvas from "@/components/canvas/canvas";
+import usePersistentState from "@/components/canvas/usePersistentState";
 
 const Game: NextPageWithLayout = () => {
 	const [gameRooms, setGameRooms] = useState([]);
-	const [onGame, setOnGame] = useState(false);
+	const [onGame, setOnGame] = usePersistentState('onGame', false);
 	const { socket } = useContext(GameSocketContext);
-	// socketio 로 자동 매칭 요청
-	const handleMatching = () => {
-		if (socket) {
-			socket.emit('postMatching');
-		}
-	}
+	const [match, setMatch] = useState('자동 매칭');
 
 	// socketio 로 게임방 목록 요청
 	useEffect(() => {
@@ -28,26 +24,62 @@ const Game: NextPageWithLayout = () => {
 			})
 			socket.on('getMatching', (data) => {
 				console.log(`매칭되었습니당: ${data}`);
+				setMatch('자동 매칭');
 				// TODO
 				// 매칭되면 게임방으로 이동
 				setOnGame(true);
 			})
+			socket.on('getCancelMatching', (data) => {
+				console.log(`매칭 취소되었습니당: ${data}`);
+				setMatch('자동 매칭');
+			})
+			socket.on('postLeaveGame', (data) => {
+        console.log('getLeaveGame: ', data);
+        if (data == 'delete') {
+					socket.emit('postLeaveGame');
+        } else if (data == 'leave') {
+					setOnGame(false);
+        }
+      })
+			socket.on('finishGame', () => {
+				setOnGame(false);
+			})
 			// socket.on('gaming', (data) => {
-      //   console.log(data);
-      // })
-			// console.log('getGameList', socket)
-			socket.emit('getGameList');
-		}
-	}, [socket])
+				//   console.log(data);
+				// })
+				// console.log('getGameList', socket)
+				socket.emit('getGameList'); // 이거 삭제 해야 하나?
+			}
+		}, [socket])
 
-	return (
-		onGame ? (
+		// socketio 로 자동 매칭 요청
+		const handleMatching = () => {
+			if (socket) {
+				if (match == '자동 매칭') {
+					socket.emit('postMatching');
+					setMatch('매칭 중~~~');
+				} else {
+					socket.emit('postCancelMatching');
+					setMatch('자동 매칭');
+				}
+			}
+		}
+
+		const hadleWatching = () => {
+			if (socket) {
+				socket.emit('postWatching');
+				setOnGame(true);
+			}
+		}
+
+		return (
+			onGame ? (
 			<Canvas></Canvas>
-		) : (
-			<div className="flex h-full w-full flex-col items-center px-8 py-6">
+			) : (
+				<div className="flex h-full w-full flex-col items-center px-8 py-6">
 				{/* 자동 매칭 버튼 */}
 				<button onClick={handleMatching} className="bg-zinc-400 w-[50%] text-black h-20 rounded-lg hover:bg-white hover:text-zinc-800" >
-					자동 매칭
+					{match}
 				</button>
 				{/* 게임방 목록 */}
 				<div className="container mx-auto py-6">
@@ -59,10 +91,15 @@ const Game: NextPageWithLayout = () => {
 						{gameRooms.map((room, index) => (
 							<div key={index} className="bg-zinc-800 text-white p-4 rounded-lg shadow">
 								<div className="flex justify-between items-center px-10">
-									<span className="font-bold">{room.roomId}</span>
+									<span className="font-bold">{room.title}</span>
 									<span>{room.status}</span>
 									{/* <span>{room.wa} players</span> */}
-									<button className="rounded-lg bg-zinc-400 p-3 hover:bg-zinc-700 transition-colors cursor-pointer">관전하기</button>
+									<button 
+										onClick={hadleWatching}
+										className="rounded-lg bg-zinc-400 p-3 hover:bg-zinc-700 transition-colors cursor-pointer"
+									>
+										관전하기
+									</button>
 								</div>
 							</div>
 						))}
