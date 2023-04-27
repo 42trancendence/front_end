5ade726d9ac99b26a61648b7aa180c0cbed53957
objectio <objection@kakao.com>
2023-04-27 15:11:10 +0900

    [ADD] upload index.tsx

--- pages/lobby/game/index.tsx
+++ pages/lobby/game/index.tsx
@@ -4,6 +4,7 @@ export default function GameRoom({ pageProps }: { pageProps?: any }) {
 	return (
 		<Layout pageProps={pageProps}>
 			<div className="flex h-full w-full flex-col items-center px-8 py-6">
+				aaa
 				<div className=""></div>
 			</div>
 		</Layout>
