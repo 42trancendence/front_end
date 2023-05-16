e5bc18df89bd7a4c130ff39ce4e3c57169e587e3
Leafmire <ekwkqdk007@naver.com>
2023-05-16 20:16:04 +0900

    [ADD] 소켓을 위한 location block 추가

--- components/BannedChatModal.tsx
+++ components/BannedChatModal.tsx
@@ -45,13 +45,15 @@ export default function BannedChatModal({
 
 	return userData.map((user: any, index: number) => (
 		me ?
-		<Menu as="li" key={index}>
+		<Menu as="li" key={index} className={clsx(!user.isEntered && "opacity-50", "group")}>
 			<div className="bg-black"></div>
 			<Menu.Button className="group flex w-full items-center gap-x-4 rounded-md p-2 text-sm font-normal leading-6 text-indigo-200 hover:bg-zinc-700 hover:text-white">
-				<Image
-					className="inline-block h-7 w-7 flex-none rounded-full"
-					src={DefaultAvatarPic}
+			<Image
+					className="inline-block h-6 w-6 flex-none rounded-full"
+					src={user.user.avatarImageUrl}
 					alt=""
+					width={28}
+					height={28}
 				/>
 				<span className="mr-auto">{user.user.name}</span>
 				{user.role === 0 ? (
