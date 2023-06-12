10a23f3ae5b06321a4dce3a973a543bfe05d4653
Leafmire <ekwkqdk007@naver.com>
2023-06-12 23:09:23 +0900

    [ADD] env 리스트 목록 파일로 생성

--- next.config.js
+++ next.config.js
@@ -17,10 +17,6 @@ const nextConfig = {
     ],
   },
   typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    // !! WARN !!
     ignoreBuildErrors: true,
   },
 }
